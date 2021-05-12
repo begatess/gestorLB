201f941a4e535de98134a7eec4ea731ec799881e
B. Erendira <be.ricar18@gmail.com>
2021-05-12 00:35:34 -0500

    Update func.js

--- js/func.js
+++ js/func.js
@@ -61,19 +61,3 @@ $(document).on("click","#eliminar",function(){
       	}
 	});
 })
-function guarda1()
-    {
-        $.ajax({
-            type:'POST', //aqui puede ser igual get
-            url: 'Encargados/guardaPrestamo.php',//aqui va tu direccion donde esta tu funcion php
-            data: {nombre: nameP, matricula: matriculaP, numero: numeroP, material: material, coment: comentarioPrestamo},//aqui tus datos
-            success:function(){
-                //lo que devuelve tu archivo mifuncion.php
-				alert("si entre");
-           },
-           error:function(){
-			alert('There was some error performing the AJAX call!');
-            //lo que devuelve si falla tu archivo mifuncion.php
-           }
-         });
-    }
